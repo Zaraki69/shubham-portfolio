879bd7cdc5bd11a33705312e9a23c6411b990294
Shubham Gajjewar <shubhamgajjewar@gmail.com>
2026-03-08 18:30:23 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var typed = new Typed('#typing', {
+
+strings: [
+"DevOps Engineer",
+"AWS Specialist",
+"Kubernetes Enthusiast",
+"Automation Builder"
+],
+
+typeSpeed:60,
+backSpeed:40,
+loop:true
+
+});
